2f2f98c51378ae78d85a3a212be08f51f0cdd03c
released <released@users.noreply.github.com>
2021-08-27 15:48:54 +0800

    Initial commit

---
+++ SampleCode/Template/main.c
@@ -0,0 +1,593 @@
+/*_____ I N C L U D E S ____________________________________________________*/
+#include <stdio.h>
+#include <string.h>
+#include "NuMicro.h"
+
+#include	"project_config.h"
+
+#include "usbh_lib.h"
+#include "ff.h"
+#include "diskio.h"
+
+#include "usbh_hid.h"
+
+/*_____ D E C L A R A T I O N S ____________________________________________*/
+
+FILINFO Finfo;
+
+volatile uint32_t  g_tick_cnt;
+uint32_t           g_t0;
+
+HID_DEV_T   *g_hid_list[CONFIG_HID_MAX_DEV];
+
+TCHAR   usbh_path[] = { '3', ':', 0 };
+/*_____ D E F I N I T I O N S ______________________________________________*/
+volatile uint32_t BitFlag = 0;
+volatile uint32_t counter_tick = 0;
+
+
+/*_____ M A C R O S ________________________________________________________*/
+
+/*_____ F U N C T I O N S __________________________________________________*/
+
+void tick_counter(void)
+{
+	counter_tick++;
+}
+
+uint32_t get_tick(void)
+{
+	return (counter_tick);
+}
+
+void set_tick(uint32_t t)
+{
+	counter_tick = t;
+}
+
+void compare_buffer(uint8_t *src, uint8_t *des, int nBytes)
+{
+    uint16_t i = 0;	
+	
+    for (i = 0; i < nBytes; i++)
+    {
+        if (src[i] != des[i])
+        {
+            printf("error idx : %4d : 0x%2X , 0x%2X\r\n", i , src[i],des[i]);
+			set_flag(flag_error , ENABLE);
+        }
+    }
+
+	if (!is_flag_set(flag_error))
+	{
+    	printf("%s finish \r\n" , __FUNCTION__);	
+		set_flag(flag_error , DISABLE);
+	}
+
+}
+
+void reset_buffer(void *dest, unsigned int val, unsigned int size)
+{
+    uint8_t *pu8Dest;
+//    unsigned int i;
+    
+    pu8Dest = (uint8_t *)dest;
+
+	#if 1
+	while (size-- > 0)
+		*pu8Dest++ = val;
+	#else
+	memset(pu8Dest, val, size * (sizeof(pu8Dest[0]) ));
+	#endif
+	
+}
+
+void copy_buffer(void *dest, void *src, unsigned int size)
+{
+    uint8_t *pu8Src, *pu8Dest;
+    unsigned int i;
+    
+    pu8Dest = (uint8_t *)dest;
+    pu8Src  = (uint8_t *)src;
+
+
+	#if 0
+	  while (size--)
+	    *pu8Dest++ = *pu8Src++;
+	#else
+    for (i = 0; i < size; i++)
+        pu8Dest[i] = pu8Src[i];
+	#endif
+}
+
+void dump_buffer(uint8_t *pucBuff, int nBytes)
+{
+    uint16_t i = 0;
+    
+    printf("dump_buffer : %2d\r\n" , nBytes);    
+    for (i = 0 ; i < nBytes ; i++)
+    {
+        printf("0x%2X," , pucBuff[i]);
+        if ((i+1)%8 ==0)
+        {
+            printf("\r\n");
+        }            
+    }
+    printf("\r\n\r\n");
+}
+
+void  dump_buffer_hex(uint8_t *pucBuff, int nBytes)
+{
+    int     nIdx, i;
+
+    nIdx = 0;
+    while (nBytes > 0)
+    {
+        printf("0x%04X  ", nIdx);
+        for (i = 0; i < 16; i++)
+            printf("%02X ", pucBuff[nIdx + i]);
+        printf("  ");
+        for (i = 0; i < 16; i++)
+        {
+            if ((pucBuff[nIdx + i] >= 0x20) && (pucBuff[nIdx + i] < 127))
+                printf("%c", pucBuff[nIdx + i]);
+            else
+                printf(".");
+            nBytes--;
+        }
+        nIdx += 16;
+        printf("\n");
+    }
+    printf("\n");
+}
+
+
+int  is_a_new_hid_device(HID_DEV_T *hdev)
+{
+    int    i;
+    for (i = 0; i < CONFIG_HID_MAX_DEV; i++)
+    {
+        if ((g_hid_list[i] != NULL) && (g_hid_list[i] == hdev) &&
+                (g_hid_list[i]->uid == hdev->uid))
+            return 0;
+    }
+    return 1;
+}
+
+void update_hid_device_list(HID_DEV_T *hdev)
+{
+    int  i = 0;
+    memset(g_hid_list, 0, sizeof(g_hid_list));
+    while ((i < CONFIG_HID_MAX_DEV) && (hdev != NULL))
+    {
+        g_hid_list[i++] = hdev;
+        hdev = hdev->next;
+    }
+}
+
+void  int_read_callback(HID_DEV_T *hdev, uint16_t ep_addr, int status, uint8_t *rdata, uint32_t data_len)
+{
+    /* This callback is in interrupt context! */
+    /*
+     *  USB host HID driver notify user the transfer status via <status> parameter. If the
+     *  If <status> is 0, the USB transfer is fine. If <status> is not zero, this interrupt in
+     *  transfer failed and HID driver will stop this pipe. It can be caused by USB transfer error
+     *  or device disconnected.
+     */
+    if (status < 0)
+    {
+        printf("Interrupt in transfer failed! status: %d\n", status);
+        return;
+    }
+    printf("Device [0x%x,0x%x] ep 0x%x, %d bytes received =>\n",
+           hdev->idVendor, hdev->idProduct, ep_addr, data_len);
+    dump_buffer_hex(rdata, data_len);
+}
+
+
+int  init_hid_device(HID_DEV_T *hdev)
+{
+    int     ret;
+
+    printf("\n\n==================================\n");
+    printf("  Init HID device : 0x%x\n", (int)hdev);
+    printf("  VID: 0x%x, PID: 0x%x\n\n", hdev->idVendor, hdev->idProduct);
+
+
+    printf("  bSubClassCode: 0x%x, bProtocolCode: 0x%x\n\n", hdev->bSubClassCode, hdev->bProtocolCode);
+    //ret = usbh_hid_set_idle(hdev, 0, 0);
+    //if (ret != HID_RET_OK)
+    //  printf("HID Set_Idle command failed! %d\n", ret);
+
+    printf("\nUSBH_HidStartIntReadPipe...\n");
+    ret = usbh_hid_start_int_read(hdev, 0, int_read_callback);
+    if (ret != HID_RET_OK)
+        printf("usbh_hid_start_int_read failed! %d\n", ret);
+    else
+        printf("Interrupt in transfer started...\n");
+
+    return 0;
+}
+
+/*
+ *  Mouse callback function
+ */
+void mouse_callback(struct usbhid_dev *hdev, MOUSE_EVENT_T *mouse)
+{
+    /* This callback is in interrupt context! */
+
+    printf("X: %d, Y: %d, W: %d, button: 0x%x\n", mouse->X, mouse->Y, mouse->wheel, mouse->button_map);
+}
+
+
+
+void SysTick_Handler(void)
+{
+    g_tick_cnt++;
+}
+
+void enable_sys_tick(int ticks_per_second)
+{
+    g_tick_cnt = 0;
+    if (SysTick_Config(SystemCoreClock / ticks_per_second))
+    {
+        /* Setup SysTick Timer for 1 second interrupts  */
+        printf("Set system tick error!!\n");
+        while (1);
+    }
+}
+
+
+uint32_t get_ticks()
+{
+    return g_tick_cnt;
+}
+
+void timer_init()
+{
+    g_t0 = get_ticks();
+}
+
+uint32_t get_timer_value()
+{
+    return (get_ticks() - g_t0);
+}
+
+/*
+ *  This function is necessary for USB Host library.
+ */
+void delay_us(int usec)
+{
+    /*
+     *  Configure Timer0, clock source from XTL_12M. Prescale 12
+     */
+    /* TIMER0 clock from HXT */
+    CLK->CLKSEL1 = (CLK->CLKSEL1 & (~CLK_CLKSEL1_TMR0SEL_Msk)) | CLK_CLKSEL1_TMR0SEL_HXT;
+    CLK->APBCLK0 |= CLK_APBCLK0_TMR0CKEN_Msk;
+    TIMER0->CTL = 0;        /* disable timer */
+    TIMER0->INTSTS = 0x3;   /* write 1 to clear for safty */
+    TIMER0->CMP = usec;
+    TIMER0->CTL = (11 << TIMER_CTL_PSC_Pos) | TIMER_ONESHOT_MODE | TIMER_CTL_CNTEN_Msk;
+
+    while (!TIMER0->INTSTS);
+}
+
+unsigned long get_fattime (void)
+{
+    unsigned long tmr;
+    tmr=0x00000;
+    return tmr;
+}
+
+int  do_dir(void)
+{
+    long    p1;                             /* total file size counter                    */
+    FRESULT res;                            /* FATFS operation return code                */
+
+    DIR dir;                                /* FATFS directory object                     */
+    UINT s1, s2;                            /* file and directory counter                 */
+
+    if (f_opendir(&dir, usbh_path))         /* try to open USB drive root directory       */
+        return -1;                          /* open failed                                */
+
+    p1 = s1 = s2 = 0;                       /* initialize counters                        */
+    for (; ;)                               /* loop until reached end of root directory   */
+    {
+        res = f_readdir(&dir, &Finfo);      /* read directory entry                       */
+        if ((res != FR_OK) || !Finfo.fname[0]) break;  /* no more entries                 */
+        if (Finfo.fattrib & AM_DIR)         /* is a directory?                            */
+        {
+            s2++;                           /* increase directory counter                 */
+        }
+        else                                /* should be a file                           */
+        {
+            s1++;                           /* increase file counter                      */
+            p1 += Finfo.fsize;              /* increase total file size counter           */
+        }
+        /* print file entry information               */
+        printf("%c%c%c%c%c %u/%02u/%02u %02u:%02u %9lu  %s",
+               (Finfo.fattrib & AM_DIR) ? 'D' : '-',    /* is a directory?                */
+               (Finfo.fattrib & AM_RDO) ? 'R' : '-',    /* is read-only?                  */
+               (Finfo.fattrib & AM_HID) ? 'H' : '-',    /* is hidden?                     */
+               (Finfo.fattrib & AM_SYS) ? 'S' : '-',    /* is system file/directory?      */
+               (Finfo.fattrib & AM_ARC) ? 'A' : '-',    /* is an archive?                 */
+               (Finfo.fdate >> 9) + 1980, (Finfo.fdate >> 5) & 15, Finfo.fdate & 31,       /* date */
+               (Finfo.ftime >> 11), (Finfo.ftime >> 5) & 63, Finfo.fsize, Finfo.fname);    /* time */
+#if _USE_LFN
+        for (p2 = strlen(Finfo.fname); p2 < 14; p2++)   /* print the long file name       */
+            putchar(' ');
+        printf("%s\n", Lfname);
+#else
+        putchar('\n');
+#endif
+    }
+    /* print the statistic information            */
+    printf("%4u File(s),%10lu bytes total\n%4u Dir(s)", s1, p1, s2);
+    return 0;
+}
+
+void USBH_HID_Init(void)
+{
+    usbh_hid_init();
+
+    usbh_hid_regitser_mouse_callback(mouse_callback);
+
+    memset(g_hid_list, 0, sizeof(g_hid_list));
+}
+
+void USBH_Init(void)
+{
+    usbh_core_init();
+	
+}
+
+void TMR1_IRQHandler(void)
+{
+//	static uint32_t LOG = 0;
+
+	
+    if(TIMER_GetIntFlag(TIMER1) == 1)
+    {
+        TIMER_ClearIntFlag(TIMER1);
+		tick_counter();
+
+		if ((get_tick() % 1000) == 0)
+		{
+//        	printf("%s : %4d\r\n",__FUNCTION__,LOG++);
+			PH0 ^= 1;
+		}
+
+		if ((get_tick() % 50) == 0)
+		{
+
+		}	
+    }
+}
+
+
+void TIMER1_Init(void)
+{
+    TIMER_Open(TIMER1, TIMER_PERIODIC_MODE, 1000);
+    TIMER_EnableInt(TIMER1);
+    NVIC_EnableIRQ(TMR1_IRQn);	
+    TIMER_Start(TIMER1);
+}
+
+void UARTx_Process(void)
+{
+	uint8_t res = 0;
+	res = UART_READ(UART0);
+
+	if (res == 'x' || res == 'X')
+	{
+		NVIC_SystemReset();
+	}
+
+	if (res > 0x7F)
+	{
+		printf("invalid command\r\n");
+	}
+	else
+	{
+		switch(res)
+		{
+			case '1':
+				break;
+
+			case 'X':
+			case 'x':
+			case 'Z':
+			case 'z':
+				NVIC_SystemReset();		
+				break;
+		}
+	}
+}
+
+
+void UART0_IRQHandler(void)
+{
+    if(UART_GET_INT_FLAG(UART0, UART_INTSTS_RDAINT_Msk | UART_INTSTS_RXTOINT_Msk))     /* UART receive data available flag */
+    {
+        while(UART_GET_RX_EMPTY(UART0) == 0)
+        {
+			UARTx_Process();
+        }
+    }
+
+    if(UART0->FIFOSTS & (UART_FIFOSTS_BIF_Msk | UART_FIFOSTS_FEF_Msk | UART_FIFOSTS_PEF_Msk | UART_FIFOSTS_RXOVIF_Msk))
+    {
+        UART_ClearIntFlag(UART0, (UART_INTSTS_RLSINT_Msk| UART_INTSTS_BUFERRINT_Msk));
+    }	
+}
+
+void UART0_Init(void)
+{
+    SYS_ResetModule(UART0_RST);
+
+    /* Configure UART0 and set UART0 baud rate */
+    UART_Open(UART0, 115200);
+
+	/* Set UART receive time-out */
+	UART_SetTimeoutCnt(UART0, 20);
+
+	UART0->FIFO &= ~UART_FIFO_RFITL_4BYTES;
+	UART0->FIFO |= UART_FIFO_RFITL_8BYTES;
+
+	/* Enable UART Interrupt - */
+	UART_ENABLE_INT(UART0, UART_INTEN_RDAIEN_Msk | UART_INTEN_TOCNTEN_Msk | UART_INTEN_RXTOIEN_Msk);
+	
+	NVIC_EnableIRQ(UART0_IRQn);
+
+	#if (_debug_log_UART_ == 1)	//debug
+	printf("\r\nCLK_GetCPUFreq : %8d\r\n",CLK_GetCPUFreq());
+	printf("CLK_GetHXTFreq : %8d\r\n",CLK_GetHXTFreq());
+	printf("CLK_GetLXTFreq : %8d\r\n",CLK_GetLXTFreq());	
+	printf("CLK_GetPCLK0Freq : %8d\r\n",CLK_GetPCLK0Freq());
+	printf("CLK_GetPCLK1Freq : %8d\r\n",CLK_GetPCLK1Freq());	
+
+//    printf("Product ID 0x%8X\n", SYS->PDID);
+	
+	#endif
+}
+
+void Custom_Init(void)
+{
+	SYS->GPH_MFPL = (SYS->GPH_MFPL & ~(SYS_GPH_MFPL_PH0MFP_Msk)) | (SYS_GPH_MFPL_PH0MFP_GPIO);
+	SYS->GPH_MFPL = (SYS->GPH_MFPL & ~(SYS_GPH_MFPL_PH1MFP_Msk)) | (SYS_GPH_MFPL_PH1MFP_GPIO);
+	SYS->GPH_MFPL = (SYS->GPH_MFPL & ~(SYS_GPH_MFPL_PH2MFP_Msk)) | (SYS_GPH_MFPL_PH2MFP_GPIO);
+
+	//EVM LED
+	GPIO_SetMode(PH,BIT0,GPIO_MODE_OUTPUT);
+	GPIO_SetMode(PH,BIT1,GPIO_MODE_OUTPUT);
+	GPIO_SetMode(PH,BIT2,GPIO_MODE_OUTPUT);
+	
+}
+
+void SYS_Init(void)
+{
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Set XT1_OUT(PF.2) and XT1_IN(PF.3) to input mode */
+    PF->MODE &= ~(GPIO_MODE_MODE2_Msk | GPIO_MODE_MODE3_Msk);
+
+    CLK_EnableXtalRC(CLK_PWRCTL_HIRCEN_Msk);
+    CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
+
+    CLK_EnableXtalRC(CLK_PWRCTL_HXTEN_Msk);
+    CLK_WaitClockReady(CLK_STATUS_HXTSTB_Msk);
+
+//    CLK_EnableXtalRC(CLK_PWRCTL_LIRCEN_Msk);
+//    CLK_WaitClockReady(CLK_STATUS_LIRCSTB_Msk);
+
+//    CLK_EnableXtalRC(CLK_PWRCTL_LXTEN_Msk);
+//    CLK_WaitClockReady(CLK_STATUS_LXTSTB_Msk);
+
+    /* Switch HCLK clock source to HXT */
+    CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_HXT,CLK_CLKDIV0_HCLK(1));
+
+    /* Set core clock as PLL_CLOCK from PLL */
+    CLK_SetCoreClock(FREQ_192MHZ);
+    /* Set PCLK0/PCLK1 to HCLK/2 */
+    CLK->PCLKDIV = (CLK_PCLKDIV_APB0DIV_DIV2 | CLK_PCLKDIV_APB1DIV_DIV2);
+
+    /* Enable UART clock */
+    CLK_EnableModuleClock(UART0_MODULE);
+    /* Select UART clock source from HXT */
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART0SEL_HIRC, CLK_CLKDIV0_UART0(1));
+
+    /* Set GPB multi-function pins for UART0 RXD and TXD */
+    SYS->GPB_MFPH &= ~(SYS_GPB_MFPH_PB12MFP_Msk | SYS_GPB_MFPH_PB13MFP_Msk);
+    SYS->GPB_MFPH |= (SYS_GPB_MFPH_PB12MFP_UART0_RXD | SYS_GPB_MFPH_PB13MFP_UART0_TXD);
+
+    CLK_EnableModuleClock(TMR1_MODULE);
+    CLK_SetModuleClock(TMR1_MODULE, CLK_CLKSEL1_TMR1SEL_HIRC, 0);
+
+
+
+    /* Enable USBH module clock */
+    CLK_EnableModuleClock(USBH_MODULE);
+
+    /* USB Host desired input clock is 48 MHz. Set as PLL divided by 4 (192/4 = 48) */
+    CLK->CLKDIV0 = (CLK->CLKDIV0 & ~CLK_CLKDIV0_USBDIV_Msk) | CLK_CLKDIV0_USB(4);
+
+    /* Enable USBD and OTG clock */
+    CLK->APBCLK0 |= CLK_APBCLK0_USBDCKEN_Msk | CLK_APBCLK0_OTGCKEN_Msk;
+
+    /* Set OTG as USB Host role */
+    SYS->USBPHY = SYS_USBPHY_HSUSBEN_Msk | (0x1 << SYS_USBPHY_HSUSBROLE_Pos) | SYS_USBPHY_USBEN_Msk | SYS_USBPHY_SBO_Msk | (0x1 << SYS_USBPHY_USBROLE_Pos);
+    delay_us(20);
+    SYS->USBPHY |= SYS_USBPHY_HSUSBACT_Msk;
+
+
+    /* HSUSB_VBUS_EN (USB 2.0 VBUS power enable pin) multi-function pin - PB.10   */
+    SYS->GPB_MFPH = (SYS->GPB_MFPH & ~SYS_GPB_MFPH_PB10MFP_Msk) | SYS_GPB_MFPH_PB10MFP_HSUSB_VBUS_EN;
+
+    /* HSUSB_VBUS_ST (USB 2.0 over-current detect pin) multi-function pin - PB.11 */
+    SYS->GPB_MFPH = (SYS->GPB_MFPH & ~SYS_GPB_MFPH_PB11MFP_Msk) | SYS_GPB_MFPH_PB11MFP_HSUSB_VBUS_ST;
+	
+    /* Update System Core Clock */
+    /* User can use SystemCoreClockUpdate() to calculate SystemCoreClock. */
+    SystemCoreClockUpdate();
+
+    /* Lock protected registers */
+    SYS_LockReg();
+}
+
+/*
+ * This is a template project for M480 series MCU. Users could based on this project to create their
+ * own application without worry about the IAR/Keil project settings.
+ *
+ * This template application uses external crystal as HCLK source and configures UART0 to print out
+ * "Hello World", users may need to do extra system configuration based on their system design.
+ */
+
+int main()
+{
+    HID_DEV_T    *hdev, *hdev_list;
+	
+    SYS_Init();
+
+	UART0_Init();
+	Custom_Init();	
+	TIMER1_Init();
+
+	USBH_Init();
+    enable_sys_tick(100);
+
+	usbh_umas_init();
+	USBH_HID_Init();
+
+    /* Got no where to go, just loop forever */
+    while(1)
+    {
+        if (usbh_pooling_hubs())             /* USB Host port detect polling and management */
+        {
+//            usbh_memory_used();              /* print out USB memory allocating information */
+//            printf("\n Has hub events.\n");
+
+            hdev_list = usbh_hid_get_device_list();
+
+            hdev = hdev_list;
+            while (hdev != NULL)
+            {
+                if (is_a_new_hid_device(hdev))
+                {
+                    init_hid_device(hdev);
+                }
+                hdev = hdev->next;
+            }
+
+            update_hid_device_list(hdev_list);
+
+		    if (f_chdrive(usbh_path) == FR_OK)
+		    {
+				do_dir();
+		    }	
+			
+        }
+    }
+}
+
+/*** (C) COPYRIGHT 2016 Nuvoton Technology Corp. ***/
